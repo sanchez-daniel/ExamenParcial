0d77ec7360eba5c3020ee38a5e7ab41ac65c7ce4
Daniel Sanchez <daniel.asanchezm@unac.edu.co>
2022-04-19 21:16:55 -0500

    nuevo proyecto

---
+++ JavaScrip.js
@@ -0,0 +1,5 @@
+function calcular(){
+    let Valor1 = parseInt(document.getElementById("punto1").value)
+    let Valor2 = parseInt(document.getElementById("punto2").value)
+    let resultado = Valor1 + Valor2
+}
